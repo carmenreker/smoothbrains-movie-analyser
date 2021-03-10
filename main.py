9084488941f83ac47fad7d0ee4769a70103553ff
TaedeM <taede.meijer@gmail.com>
2021-03-10 15:17:05 +0100

    pycodestyle op main.py gegooid

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # Filename: main.py
 # Description: opens the files and calls the other programs
-# Author: Taede Meijer (als je hier iets aan verandert hebt ff bij zetten je weet toch)
+# Author: Taede Meijer (als je hier iets aan verandert hebt ff bij zetten toch)
+
 
 def main():
-    with open ('movies/mission_impossible/mi.txt', 'r') as script:
-        with open ('movies/mission_impossible/mi.srt', 'r') as subtitles:
+    with open('movies/mission_impossible/mi.txt', 'r') as script:
+        with open('movies/mission_impossible/mi.srt', 'r') as subtitles:
             for line in subtitles:
                 print(line)
-  
-    
+
 
 if __name__ == "__main__":
     main()
