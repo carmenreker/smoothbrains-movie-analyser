7453bc68f80985309ce1090326569cc52bd8f009
TaedeM <taede.meijer@gmail.com>
2021-04-06 16:37:38 +0200

    Timestamp + text werkend

--- compare.py
+++ compare.py
@@ -1,31 +1,11 @@
 import re
+import pysrt
 
 def loadsubtitles():
-    #print("esketit")
-    with open('movies/mission_impossible/mi.srt') as sub:
-        test = sub.read()
-        test = re.sub('<.*?>','', test)
-        test = re.sub('- ', '', test)
-        print(test)
-        print("------------------------------------")
-        esketit = re.findall('[0-9][0-9][:][0-9][0-9][:].*[\n\n]', test)
-        
-        poep = re.findall('[0-9]*[- ]*["]*[a-zA-Z].+[^\s*$]', test)
-
-        print('esketit =', + len(esketit))
-
-        print('poep =', + len(poep))
-        print("------------------------------------")
-        print("------------------------------------")
-
-        for i in range(30):
-            print(esketit[i])
-            print(poep[i])
+    subs = pysrt.open('testfiles/testsubtitles.srt')
+    
+    for subtitle in subs:      
+        print(subtitle.start)
+        print(subtitle.text)
 
 loadsubtitles()
-
-# [0-9]*[- ]*["]*[a-zA-Z].+[^\s*$]
-# Die doet het half
-
-# Timestaps 
-# \d{2}:\d{2}:\d{2},\d{3}.+[\n]
