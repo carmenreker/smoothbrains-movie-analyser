65b190a43c0a08c8fb25453bf9f69869d093b920
TaedeM <taede.meijer@gmail.com>
2021-04-06 15:52:24 +0200

    esketit

--- compare.py
+++ compare.py
@@ -1,7 +1,31 @@
+import re
+
 def loadsubtitles():
-    print("esketit")
+    #print("esketit")
     with open('movies/mission_impossible/mi.srt') as sub:
-        lines = sub.readlines()
-        print(lines)
+        test = sub.read()
+        test = re.sub('<.*?>','', test)
+        test = re.sub('- ', '', test)
+        print(test)
+        print("------------------------------------")
+        esketit = re.findall('[0-9][0-9][:][0-9][0-9][:].*[\n\n]', test)
+        
+        poep = re.findall('[0-9]*[- ]*["]*[a-zA-Z].+[^\s*$]', test)
+
+        print('esketit =', + len(esketit))
+
+        print('poep =', + len(poep))
+        print("------------------------------------")
+        print("------------------------------------")
+
+        for i in range(30):
+            print(esketit[i])
+            print(poep[i])
 
 loadsubtitles()
+
+# [0-9]*[- ]*["]*[a-zA-Z].+[^\s*$]
+# Die doet het half
+
+# Timestaps 
+# \d{2}:\d{2}:\d{2},\d{3}.+[\n]
