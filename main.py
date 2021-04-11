e24078dc2c01eb0300e471c7afb745d068f345d1
TaedeM <taede.meijer@gmail.com>
2021-04-11 16:50:58 +0200

    Character names and matching lines are now returned as 2 lists of same size

--- main.py
+++ main.py
@@ -5,65 +5,72 @@
 from loadsubtitle import loadsubtitles
 from timestamps import add_timestamps
 from labels import create_labels
+from names import add_names
 import argparse
 
 def match(script, subtitles):
-	"""Compares the subtitles to the script, and prints the percentage
-	   of matches"""
+    """Compares the subtitles to the script, and prints the percentage
+       of matches"""
 
-	counter_not_in_script = 0
-	counter_is_in_script = 0
+    counter_not_in_script = 0
+    counter_is_in_script = 0
 
-	# loopt over elke subtitle tekst in de dictionary
-	for line in subtitles:
+    # loopt over elke subtitle tekst in de dictionary
+    for line in subtitles:
 
-		# checkt of de subtitle tekst ergens in het script staat (dit is nog te vaag)
-		if line in script:
+        # checkt of de subtitle tekst ergens in het script staat (dit is nog te vaag)
+        if line in script:
 
-			# Uncomment deze twee als je de matches wil printen.
-			#print("Match: ", end="")
-			#print(line)
-			counter_is_in_script += 1
+            # Uncomment deze twee als je de matches wil printen.
+            #print("Match: ", end="")
+            #print(line)
+            counter_is_in_script += 1
 
-		else:
-			counter_not_in_script += 1	  
-	
-	print("De ondertiteling matcht voor {}{} met het script".format(round(counter_is_in_script / (counter_is_in_script + counter_not_in_script) * 100, 2), "%"))
+        else:
+            counter_not_in_script += 1      
+    
+    print("De ondertiteling matcht voor {}{} met het script".format(round(counter_is_in_script / (counter_is_in_script + counter_not_in_script) * 100, 2), "%"))
 
 def main():
 
-	parser = argparse.ArgumentParser(
-									description="This programme aligns subtitles and \
-									movie scripts, output is in csv.",
-									usage="Enter a subtitle (.srt) and a movie script (.txt) file, \
-									in this order, for the programme to align.")
-	parser.add_argument(
-						"subtitleFile", metavar="subtitle file",
-						help="the file that contains the subtitles (.srt extention).",
-						action="append", nargs="+")
-	parser.add_argument(
-						"scriptFile", metavar="movie script file",
-						help="the file that contains the movie script (.txt extention).",
-						action="append", nargs="+")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+                                    description="This programme aligns subtitles and \
+                                    movie scripts, output is in csv.",
+                                    usage="Enter a subtitle (.srt) and a movie script (.txt) file, \
+                                    in this order, for the programme to align.")
+    parser.add_argument(
+                        "subtitleFile", metavar="subtitle file",
+                        help="the file that contains the subtitles (.srt extention).",
+                        action="append", nargs="+")
+    parser.add_argument(
+                        "scriptFile", metavar="movie script file",
+                        help="the file that contains the movie script (.txt extention).",
+                        action="append", nargs="+")
+    args = parser.parse_args()
 
-	with open(args.scriptFile[0][0], 'r') as script:
-		text = script.read()
-		labelled_script = create_labels(text)
+    with open(args.scriptFile[0][0], 'r') as script:
+        text = script.read()
+        labelled_script = create_labels(text)
 
-		# Uncomment deze code om de movie te printen voor debuggen ofzo, zelf weten
-		#print(movie)
-	#subtitle_file = args.subtitleFile[0]
-	#print(str(subtitle_file))
-	subtitle = loadsubtitles(args.subtitleFile)
+        # Uncomment deze code om de movie te printen voor debuggen ofzo, zelf weten
+        #print(movie)
+    #subtitle_file = args.subtitleFile[0]
+    #print(str(subtitle_file))
+    subtitle = loadsubtitles(args.subtitleFile)
 
-	print(add_timestamps(labelled_script, subtitle))
-	# Uncomment deze code om de functie uit te voeren
-	match(labelled_script, subtitle)
+    labelled_script = add_timestamps(labelled_script, subtitle)
+    
+    # Get two lists, one with matching lines between the subtitles and script, 
+    # the other with the character names
+    matching_lines, character_names = add_names(labelled_script, subtitle)
+    #print(matching_lines, character_names)
+    
+    # Print the amount of matches between the subtitles and script
+    match(labelled_script, subtitle)
 
-	
+    
 if __name__ == "__main__":
-	main()
+    main()
 
 # Wat nog moet:
 # De juiste naam bij de subtitle zetten ?
---
+++ names.py
@@ -0,0 +1,38 @@
+import re
+
+def add_names(script, subtitles):
+
+    # We create two lists that run parallel to each other
+    # matching_lines[5] contains the line that matches
+    # and character_names[5] contains the speaker of that matching line. 
+    matching_lines = []
+    character_names = []
+
+    amount = script.count("C|")
+
+    for i in range(amount):
+        first = script.find("C|")
+
+        # Replace the first C with a placeholder, so we can locate the next 
+        script = script.replace("C|", "PLACEHOLDER", 1)
+        second = script.find("C|")
+
+        # Saves the string in between the first and second 'C|'
+        temporary_text = script[first:second]
+
+        temporary_text = temporary_text.replace("                          ", " - ")
+
+        for line in subtitles:
+
+            # If a line in the subtitles has been found in the temporary text:
+            if line in temporary_text:
+
+                matching_lines.append(line)
+
+                # Save the name of the current speaker
+                name = re.match('^PLACEHOLDER\s.+\n', temporary_text)
+                name = name.group().replace("PLACEHOLDER - ", "")
+                name = name.replace("\n", "")
+                character_names.append(name)
+
+    return matching_lines, character_names
