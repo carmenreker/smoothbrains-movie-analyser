94bdf02a55cf6719897446b880714826501c16ea
S4410653 <sijbren@DESKTOP-C1E1VP0.localdomain>
2021-04-11 14:09:32 +0200

    Pycodestyle proof

--- user_interface.py
+++ user_interface.py
@@ -4,12 +4,15 @@
 
 import argparse
 
-# def main():
-def user_interface():
+
+def main():
+	# def user_interface():
 
 	parser = argparse.ArgumentParser(
-									description="This programme aligns subtitles and movie scripts, output is in csv.",
-									usage="Enter a subtitle (.srt) and a movie script (.txt) file, in this order, for the programme to align.")
+									description="This programme aligns subtitles and \
+									movie scripts, output is in csv.",
+									usage="Enter a subtitle (.srt) and a movie script (.txt) file \
+									, in this order, for the programme to align.")
 	parser.add_argument(
 						"subtitleFile", metavar="subtitle file",
 						help="the file that contains the subtitles (.srt extention).",
@@ -19,11 +22,16 @@ def user_interface():
 						help="the file that contains the movie script (.txt extention).",
 						action="append", nargs="+")
 	args = parser.parse_args()
-# NOTE: The files are delivered in a list, so index 0 = subtitle file and index 1 = movie script file.
-# Voor de return moet je even kieke welke van de twee het handigst is voor het programma. 
-	#return args
-	#return args.subtitleFile, args.scriptFile
-	
-# Als je het programma wil checken moet je het even in een main zetten anders werkt de --help/-h  en de error handling niet :)
-# if __name__ == "__main__":
-# 	main()
+# NOTE: The files are delivered in a list, so index 0 = subtitle file
+# and index 1 = movie script file.
+# Voor de return moet je even kieke welke van de twee het handigst is
+# voor het programma.
+	# return args
+	# return args.subtitleFile, args.scriptFile
+
+# Als je het programma wil checken moet je het even in een main zetten
+# anders werkt de --help/-h  en de error handling niet :)
+
+
+if __name__ == "__main__":
+	main()
